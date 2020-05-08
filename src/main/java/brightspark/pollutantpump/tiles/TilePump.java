3eb9d9bd6a3ca527cb6024417f3db1a8a766868b
bright_spark <markpassey@btinternet.com>
2020-05-08 05:12:51 +0100

    Improved pump update logic to be more efficient

--- src/main/java/brightspark/pollutantpump/tiles/TilePump.java
+++ src/main/java/brightspark/pollutantpump/tiles/TilePump.java
@@ -3,28 +3,28 @@ package brightspark.pollutantpump.tiles;
 import brightspark.pollutantpump.PPConfig;
 import brightspark.pollutantpump.blocks.BlockPipe;
 import com.endertech.minecraft.forge.api.IPollutant;
-import com.endertech.minecraft.forge.world.ForgeWorld;
 import com.endertech.minecraft.mods.adpother.blocks.Filter;
 import com.endertech.minecraft.mods.adpother.blocks.Pollutant;
+import com.google.common.collect.Lists;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.energy.EnergyStorage;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TilePump extends TileEntity implements ITickable {
+	private final EnergyStorage energy = new EnergyStorage(PPConfig.pumpMaxEnergyStorage, Integer.MAX_VALUE);
 	private boolean checkForPipes = true;
 	private BlockPos topPipe;
-	private EnergyStorage energy = new EnergyStorage(PPConfig.pumpMaxEnergyStorage, Integer.MAX_VALUE);
 	private long lastWork;
 
 	@Override
@@ -37,60 +37,77 @@ public class TilePump extends TileEntity implements ITickable {
 		if (topPipe == null)
 			return;
 
-		if (world.getTotalWorldTime() - lastWork >= PPConfig.pumpWorkRate
-			&& energy.getEnergyStored() >= PPConfig.pumpEnergyUse) {
+		if (world.getTotalWorldTime() - lastWork >= PPConfig.pumpWorkRate && energy.getEnergyStored() >= PPConfig.pumpEnergyUse) {
 			lastWork = world.getTotalWorldTime();
 
-			//Find pollutant blocks
-			List<BlockPos> pollutantBlocks = findPollutants();
-			if (pollutantBlocks.size() <= 0)
-				return;
-
-			BlockPos pollutantPos = pollutantBlocks.get(world.rand.nextInt(pollutantBlocks.size()));
-			IBlockState pollutantState = world.getBlockState(pollutantPos);
-			Pollutant<?> pollutant = (Pollutant<?>) pollutantState.getBlock();
-			int pollutantAmount = pollutant.getCarriedPollutionAmount(pollutantState);
-
-			//Find any adjacent filters that have space
-			List<BlockPos> filters = ForgeWorld.Position.getAroundCube(world, pos, (w, p) ->
-			{
-				IBlockState state = w.getBlockState(p);
-				Block block = state.getBlock();
-				if (block instanceof Filter) {
-					Filter filter = (Filter) block;
-					Filter.BlockTile tile = filter.getBlockTile(w, p);
-					if (tile != null) {
-						int freeSpace = filter.getContent(tile).getFreeSpaceFor(pollutant);
-						return freeSpace >= pollutantAmount;
+			// Get all block positions in range
+			List<BlockPos> positions = new ArrayList<>();
+			for (int x = topPipe.getX() - PPConfig.pumpRange; x <= topPipe.getX() + PPConfig.pumpRange; x++) {
+				for (int y = topPipe.getY() - PPConfig.pumpRange; y <= topPipe.getY() + PPConfig.pumpRange; y++) {
+					for (int z = topPipe.getZ() - PPConfig.pumpRange; z <= topPipe.getZ() + PPConfig.pumpRange; z++) {
+						positions.add(new BlockPos(x, y, z));
 					}
 				}
-				return false;
-			});
+			}
+			// Shuffle the positions
+			Collections.shuffle(positions);
 
-			boolean pollutantMoved = false;
+			// Go through each position until we find a pollutant
+			for (BlockPos position : positions) {
+				IBlockState state = world.getBlockState(position);
+				Block block = state.getBlock();
 
-			int numFilters = filters.size();
-			if (numFilters > 0) {
-				//Put the pollutant into a random filter
-				BlockPos filterPos = filters.get(world.rand.nextInt(numFilters));
-				Filter filter = (Filter) ForgeWorld.getBlock(world, filterPos);
-				Filter.BlockTile tile = filter.getBlockTile(world, filterPos);
-				filter.fill(tile, pollutant, pollutantAmount);
-				pollutantMoved = true;
-			} else {
-				//Place the pollutant at a random empty pos
-				List<BlockPos> emptyPositions = ForgeWorld.Position.getAroundCube(world, pos, World::isAirBlock);
-				if (!emptyPositions.isEmpty()) {
-					BlockPos emptyPos = emptyPositions.get(world.rand.nextInt(emptyPositions.size()));
-					world.setBlockState(emptyPos, pollutantState);
-					pollutantMoved = true;
+				if (block instanceof Pollutant && ((IPollutant) block).getPollutantType() == IPollutant.Type.AIR) {
+					Pollutant<?> pollutant = (Pollutant<?>) block;
+					int pollutantAmount = pollutant.getCarriedPollutionAmount(state);
+
+					//Find any adjacent filters that have space
+					List<EnumFacing> horizontals = Lists.newArrayList(EnumFacing.HORIZONTALS);
+					Collections.shuffle(horizontals);
+
+					boolean moved = false;
+					for (EnumFacing side : horizontals) {
+						BlockPos sidePos = pos.offset(side);
+						IBlockState sideState = world.getBlockState(sidePos);
+						Block sideBlock = sideState.getBlock();
+
+						if (sideBlock instanceof Filter) {
+							Filter filter = (Filter) sideBlock;
+							Filter.BlockTile filterTE = filter.getBlockTile(world, sidePos);
+							if (filterTE != null) {
+								int freeSpace = filter.getContent(filterTE).getFreeSpaceFor(pollutant);
+								if (freeSpace >= pollutantAmount) {
+									// Move the pollutant into the filter
+									filter.fill(filterTE, pollutant, pollutantAmount);
+									moved = true;
+									break;
+								}
+							}
+						}
+					}
+
+					if (!moved) {
+						// No filters available - try move the pollutant to empty space adjacent to pump
+						for (EnumFacing side : horizontals) {
+							BlockPos sidePos = pos.offset(side);
+							if (world.isAirBlock(sidePos)) {
+								world.setBlockState(sidePos, state);
+								moved = true;
+								break;
+							}
+						}
+					}
+
+					if (moved) {
+						// If successfully moved pollutant, then set its original position to air
+						world.setBlockToAir(position);
+						break;
+					}
 				}
 			}
 
-			if (pollutantMoved) {
-				world.setBlockToAir(pollutantPos);
-				energy.extractEnergy(PPConfig.pumpEnergyUse, false);
-			}
+			// Use energy
+			energy.extractEnergy(PPConfig.pumpEnergyUse, false);
 		}
 	}
 
@@ -102,21 +119,6 @@ public class TilePump extends TileEntity implements ITickable {
 	public void clearPipe() {
 		checkForPipes = true;
 		topPipe = null;
-	}
-
-	private List<BlockPos> findPollutants() {
-		List<BlockPos> pollutants = new ArrayList<>();
-		for (int x = topPipe.getX() - PPConfig.pumpRange; x < topPipe.getX() + PPConfig.pumpRange; x++) {
-			for (int y = topPipe.getY() - PPConfig.pumpRange; y < topPipe.getY() + PPConfig.pumpRange; y++) {
-				for (int z = topPipe.getZ() - PPConfig.pumpRange; z < topPipe.getZ() + PPConfig.pumpRange; z++) {
-					BlockPos p = new BlockPos(x, y, z);
-					Block block = world.getBlockState(p).getBlock();
-					if (block instanceof Pollutant && ((IPollutant) block).getPollutantType() == IPollutant.Type.AIR)
-						pollutants.add(p);
-				}
-			}
-		}
-		return pollutants;
 	}
 
 	@SuppressWarnings("NullableProblems")
